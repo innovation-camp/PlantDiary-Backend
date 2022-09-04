6cbea68dc5874cc9182fec3ae054aa3b940e25ab
Luna <luvbliny@gmail.com>
2022-09-04 16:36:36 +0900

    Feat: init settings

---
+++ src/main/java/com/sparta/plantdiary/PlantDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.plantdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantDiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlantDiaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/plantdiary/PlantDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.plantdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantDiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
